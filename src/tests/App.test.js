fcbd926998f4c1fb0e43a145073617bfb22f359e
Larissa Perinoto <perinotolarissa@gmail.com>
2022-07-18 16:54:31 -0300

    [Project React Testing Library] - Requisito 1.

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,69 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './utils/renderWithRouter';
+import App from '../App';
+
+describe('Testa o componente App.js', () => {
+  test('Verifica se existem textos de navegação na tela', () => {
+    renderWithRouter(<App />);
+
+    const textHome = screen.getByRole('link', { name: 'Home' });
+    const textAbout = screen.getByRole('link', { name: 'About' });
+    const textFavorite = screen.getByRole('link', { name: 'Favorite Pokémons' });
+
+    expect(textHome).toBeInTheDocument();
+    expect(textAbout).toBeInTheDocument();
+    expect(textFavorite).toBeInTheDocument();
+  });
+
+  test('Verifica se redireciona para a página inicial ao clicar em "Home"', () => {
+    renderWithRouter(<App />);
+
+    const textHome = screen.getByRole('link', { name: 'Home' });
+    userEvent.click(textHome);
+
+    const title = screen.getByRole('heading', {
+      name: /Encountered pokémons/i,
+      level: 2,
+    });
+    expect(title).toBeInTheDocument();
+  });
+
+  test('Verifica se redireciona para About ao clicar em "About"', () => {
+    renderWithRouter(<App />);
+
+    const textAbout = screen.getByRole('link', { name: 'About' });
+    userEvent.click(textAbout);
+
+    const title = screen.getByRole('heading', {
+      name: /About Pokédex/i,
+      level: 2,
+    });
+    expect(title).toBeInTheDocument();
+  });
+
+  test('Verifica se redireciona para Favorites ao clicar em "Favorite Pokémons"', () => {
+    renderWithRouter(<App />);
+
+    const textFavorite = screen.getByRole('link', { name: 'Favorite Pokémons' });
+    userEvent.click(textFavorite);
+
+    const title = screen.getByRole('heading', {
+      name: /Favorite pokémons/i,
+      level: 2,
+    });
+    expect(title).toBeInTheDocument();
+  });
+
+  test('Verifica se Not Found é renderizada em outras URLs', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/pagina-que-nao-existe');
+
+    const notFound = screen.getByRole('heading', {
+      name: /Page requested not found/i,
+      level: 2,
+    });
+    expect(notFound).toBeInTheDocument();
+  });
+});
---
+++ src/tests/utils/renderWithRouter.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import { render } from '@testing-library/react';
+
+function renderWithRouter(component) {
+  const history = createMemoryHistory();
+  const renderObject = render(
+    <Router history={ history }>
+      { component }
+    </Router>,
+  );
+  return ({
+    ...renderObject, history,
+  });
+}
+
+export default renderWithRouter;
